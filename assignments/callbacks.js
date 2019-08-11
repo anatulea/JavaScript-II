b21b5af6e1f29ab93a1965a775ecd8b3d49d891d
anatulea <anatulea14@.gmali.com>
2019-08-11 00:52:30 -0500

    Completed the project

--- assignments/callbacks.js
+++ assignments/callbacks.js
@@ -4,27 +4,27 @@ const items = ['Pencil', 'Notebook', 'yo-yo', 'Gum'];
 
 
 
-  //Given this problem: 
-  // firstItem passes the first item of the given array to the callback function.
-  function firstItem(arr, callback) {
-    callback(arr[0]);
-  }
-   firstItem(items, (first) => {
-    console.log(first);
-   });
+//Given this problem: 
+// firstItem passes the first item of the given array to the callback function.
+function firstItem(arr, callback) {
+  callback(arr[0]);
+}
+firstItem(items, (first) => {
+  console.log(first);
+});
 
 
-  // Potential Solution:
+// Potential Solution:
 
-  // Higher order function using "cb" as the call back
-  function firstItem(arr, cb) {
-    return cb(arr[0]);
-  }
+// Higher order function using "cb" as the call back
+function firstItem(arr, cb) {
+  return cb(arr[0]);
+}
 
-  // Function invocation 
-  firstItem(items, function(first) {
-    console.log(first)
-  });
+// Function invocation 
+firstItem(items, function (first) {
+  console.log(first)
+});
 
 
 
@@ -41,54 +41,51 @@ getLength(items, (length) => {
 function last(arr, cb) {
   cb(arr.slice(-1)[0]);
 }
-last(items, (lastItem)=>{
+last(items, (lastItem) => {
   console.log(lastItem);
 });
 
 
 // sumNums adds two numbers (x, y) and passes the result to the callback.
 function sumNums(x, y, cb) {
-  cb(x+y);
+  cb(x + y);
 }
-sumNums(4,6, (sum)=>{
+sumNums(4, 6, (sum) => {
   console.log(sum);
 });
 
 
 // multiplyNums multiplies two numbers and passes the result to the callback.
 function multiplyNums(x, y, cb) {
-  cb(x*y);
+  cb(x * y);
 }
-multiplyNums(4,6, (multiply)=>{
+multiplyNums(4, 6, (multiply) => {
   console.log(multiply);
 });
 
 
- // contains checks if an item is present inside of the given array/list.
-  // Pass true to the callback if it is, otherwise pass false.
+// contains checks if an item is present inside of the given array/list.
+// Pass true to the callback if it is, otherwise pass false.
 function contains(item, list, cb) {
- cb(list.includes(item));
+  cb(list.includes(item));
 }
-contains("Gum", items, (finder)=>{
+contains("Gum", items, (finder) => {
   console.log(finder);
 });
 
 /* STRETCH PROBLEM */
- // removeDuplicates removes all duplicate values from the given array.
-  // Pass the duplicate free array to the callback function.
-  // Do not mutate the original array.
-let numbers = [78, 3, 56, 99, 3, 56, 77, 99.77, 87, 32, 42]
+// removeDuplicates removes all duplicate values from the given array.
+// Pass the duplicate free array to the callback function.
+// Do not mutate the original array.
 
-function removeDuplicates(array, cb) {
-  cb(numbers.map(arr, indexOf));
-}
-removeDuplicates(numbers, (remove)=>{
-  console.log(remove);
-});
 
-numbers.filter(distinct)
-for (let i = 0; i < numbers.length; i++) 
-  { for (let j = i + 1 ; j < numbers.length; j++)
-     { if (numbers[i].equals(numbers[j])) { }
-     }
-  }
+// Array.prototype.unique = function() {
+//   let values = [78, 3, 56, 99, 3, 56, 77, 99, 77, 87, 32, 42];
+ 
+//   for (let i=0, values.length, i++)
+//       if (values.indexOf(values[i]) === -1)
+//       new1.push(values[i]);
+//   return new1;
+// }
+// let new1 = [];
+// console.log(new1);
